43757c0451d3ba65786444dc2bb5acda2e2dd7e5
gyeongmun <bada6983@gmail.com>
2022-01-14 16:16:06 +0900

    create habitReset

--- src/app.jsx
+++ src/app.jsx
@@ -36,6 +36,14 @@ class App extends Component {
     const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
     this.setState({ habits });
   };
+
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  };
   render() {
     return (
       <>
@@ -48,6 +56,7 @@ class App extends Component {
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
           onAdd={this.handleAdd}
+          onReset={this.handleReset}
         />
       </>
     );
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -21,6 +21,9 @@ class Habits extends Component {
             />
           ))}
         </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
       </>
     );
   }
