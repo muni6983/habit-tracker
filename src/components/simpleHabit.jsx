e13f26144cad6aa82b8c8200230db960b0ac9f7c
gyeongmun <bada6983@gmail.com>
2022-01-16 16:06:42 +0900

    create simpleHabit-ReactHook

---
+++ src/components/simpleHabit.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useRef } from "react";
+import { useCallback } from "react";
+
+const SimpleHabit = (props) => {
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  });
+  return (
+    <li className="habit">
+      <span ref={spanRef} className="habit-name">
+        Coding
+      </span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
+export default SimpleHabit;
